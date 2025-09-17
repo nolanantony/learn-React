1bef0e54dfca354bf585e35eac2364008f2b2f1d
Nolan Dsouza <nolanantony123@gmail.com>
2025-09-17 07:29:14 +0530

    React day 2

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,7 +1,12 @@
+import Chat from "./chat.jsx"; // Importing the Chat component
 function App() {
 
   return (
+    <>
     <h1>Hello prajwal</h1>
+    <p>How are you doing? </p>
+    <Chat /> {/* Using the Chat component here */}
+    </>
   )
 }
 
---
+++ 01viteReact/src/chat.jsx
@@ -0,0 +1,7 @@
+function Chat() {
+    return (
+        <h3>Chat component</h3>
+    )
+}
+
+export default Chat;
